397cfb27bbb16a1abde160cdf449846cecce376f
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2021-03-26 10:30:29 -0400

    Use HashRouter to fix page refresh issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { IntlProvider } from "react-intl";
 import store from "./store";
@@ -11,7 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <IntlProvider locale="en">
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </IntlProvider>
     </Provider>
   </React.StrictMode>,
