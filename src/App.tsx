faf044074eca37d6b5061dcb407eb63799cabb11
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2021-08-25 02:04:32 +0000

    Add workspace dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,22 @@ import config from "./config";
 import About from "./components/About";
 import Home from "./components/Home";
 
-class App extends Component {
+type AppState = {
+  workspace: string
+}
+class App extends Component<{}, AppState> {
   title: string;
 
   constructor(props: any) {
     super(props);
     this.title = `${config.appName} - v${config.appVersion}`;
+    this.state = {
+      workspace: 'work'
+    };
   }
 
   render() {
-    const { title } = this;
+    const { title, state: { workspace } } = this;
 
     const Root = () => <Redirect to="/home" />;
 
@@ -41,6 +47,15 @@ class App extends Component {
                   Notes <small>v{config.appVersion}</small>
                 </div>
                 <ul className="nav pull-right">
+                  <li>
+                    <select
+                      value={workspace}
+                      onChange={(event) => this.setState({ workspace: event.target.value })}
+                    >
+                      <option value="work">Work</option>
+                      <option value="personal">Personal</option>
+                    </select>
+                  </li>
                   <li>
                     <NavLink
                       className="menuItem"
